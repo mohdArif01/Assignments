1a175db7c0cad6afa0049b349b47a49841e36fe6
clever_code <118424060+mohdArif01@users.noreply.github.com>
2022-12-11 15:59:44 +0530

    Add files via upload

---
+++ Print absolute number.java
@@ -0,0 +1,12 @@
+public class sunday_revision_1 {
+    public static void main(String[] args) {
+        int num = -34;
+        if (num<0){
+            num *= (-1);
+            System.out.println(num);
+        }
+        else{
+            System.out.println(num);
+        }
+    }
+}
